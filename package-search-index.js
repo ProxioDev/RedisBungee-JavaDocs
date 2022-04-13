69dbfb9cc060618264ff0cd4b33bc11db55aa0e1
mohammed jasem alaajel <xrambad@gmail.com>
2022-04-13 12:10:25 +0400

    version 0.6.5-SNAPSHOT

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.imaginarycode.minecraft.redisbungee"},{"l":"com.imaginarycode.minecraft.redisbungee.events"},{"l":"com.imaginarycode.minecraft.redisbungee.util"},{"l":"com.imaginarycode.minecraft.redisbungee.util.uuid"}];updateSearchResults();
