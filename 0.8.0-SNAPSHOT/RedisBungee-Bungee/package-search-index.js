c5757444aaa9e05223185a6d00c8b1cb30959b2e
mohammed jasem alaajel <xrambad@gmail.com>
2022-07-16 02:28:20 +0400

    0.8.0 java docs

---
+++ 0.8.0-SNAPSHOT/RedisBungee-Bungee/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.imaginarycode.minecraft.redisbungee"},{"l":"com.imaginarycode.minecraft.redisbungee.commands"},{"l":"com.imaginarycode.minecraft.redisbungee.events"}];updateSearchResults();
---
+++ 0.8.0-SNAPSHOT/index.md
@@ -0,0 +1,7 @@
+# Java docs:
+## API [Click here](./RedisBungee-API) *API ONLY NON PLATFORM DEPENDANT*
+## Bungeecord [Click here](./RedisBungee-Bungee) *includes the API*
+## Velocity [Click here](./RedisBungee-Velocity) *includes the API*
+
+
+
